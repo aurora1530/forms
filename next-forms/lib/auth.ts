d409e1041a41c76b582e73589d45208b5ab858a2
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2026-01-28 19:13:45 +0900

    feat(auth): integrate Prisma for user management on sign-in events
    - Added Prisma client to handle user data on sign-in events.
    - Implemented user upsert logic to create or update user information in the database.
    - Updated .gitignore to exclude generated Prisma files.
    - Added Prisma configuration and schema files for MongoDB.
    - Updated package.json to include Prisma dependencies.

--- next-forms/lib/auth.ts
+++ next-forms/lib/auth.ts
@@ -3,6 +3,8 @@ import Google from "@auth/core/providers/google";
 import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter";
 import { Redis } from "@upstash/redis";
 
+import prisma from '@/lib/prisma';
+
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_URL!,
   token: process.env.UPSTASH_REDIS_TOKEN!
@@ -11,6 +13,28 @@ const redis = new Redis({
 const authConfig: NextAuthConfig = {
   providers: [Google],
   adapter: UpstashRedisAdapter(redis),
-}
+  events: {
+    async signIn(message) {
+      if (!message.user.email || !message.user.name) {
+        return;
+      }
+
+      const data = {
+        email: message.user.email,
+        username: message.user.name,
+      };
+
+      try {
+        await prisma.user.upsert({
+          where: { email: message.user.email },
+          update: data,
+          create: data,
+        });
+      } catch (error) {
+        console.error('Database error', error);
+      }
+    },
+  },
+};
 
 export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
